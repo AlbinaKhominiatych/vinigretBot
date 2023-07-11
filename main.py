2422ed2bde52a4554160d3e4ab51a38f4e38f475
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-07-11 12:01:00 +0300

    додано асинхронність для багатьох запитів

--- main.py
+++ main.py
@@ -1,43 +1,67 @@
 import random
+import asyncio
+import aiohttp
 from PIL import Image, ImageDraw, ImageFont
-import telebot
-token="5954530100:AAGUkVKnI-iJXspRGG-BIqNBiwdMRYfpddo"
-#step2
-bot=telebot.TeleBot(token)
-@bot.message_handler(commands=['start'])
-def start_message(message):
-  bot.send_message(message.chat.id,"Привіт ✌\nнадішли фоточку в форматі png/jpeg, щоб створити мем")
-# step3 отримання повідомлення від юзера фото
-@bot.message_handler(content_types=['photo'])
-def photo(message):
-    #print ('message.photo =', message.photo)
-    fileID = message.photo[-1].file_id
-    #print ('fileID =', fileID)
-    file_info = bot.get_file(fileID)
-    #print ('file.file_path =', file_info.file_path)
-    downloaded_file = bot.download_file(file_info.file_path)
+from aiogram import Bot, Dispatcher, types
 
-    with open("image.jpg", 'wb') as new_file:
-        new_file.write(downloaded_file) #збереження як файлу
-    # список мемчиків зберігається в текстовому файлі
-    f = open('meme.txt', 'r', encoding='UTF-8')
-    meme = f.read().split('\n')
-    f.close()
-    image = Image.open("image.jpg")
+token = "5954530100:AAGUkVKnI-iJXspRGG-BIqNBiwdMRYfpddo"
 
-    font = ImageFont.truetype("arial", 45)
-    drawer = ImageDraw.Draw(image)
+bot = Bot(token=token)
+dp = Dispatcher(bot)
 
-    width, height = image.size
-    print(width, height)
-    drawer.text((150, height // 1.2), random.choice(meme), font=font, fill='white')
 
-    image.save('new_img.jpg')
-    #image.show()
-    bot.send_photo(message.chat.id, open('new_img.jpg', 'rb'))
+@dp.message_handler(commands=['start'])
+async def start_message(message: types.Message):
+    await message.reply("Привіт ✌\nнадішли фоточку в форматі png/jpeg, щоб створити мем")
 
-@bot.message_handler(content_types=["text"])
-def handle_text(message):
-    bot.send_message(message.chat.id, 'Та фото мені кидай)')
-#запуск бота(обов'язково)
-bot.polling()
+
+@dp.message_handler(content_types=types.ContentTypes.PHOTO)
+async def process_photo(message: types.Message):
+    photo = message.photo[-1]
+
+    # Отримання файлу зображення
+    file = await bot.get_file(photo.file_id)
+    file_path = file.file_path
+
+    # Отримання URL файлу зображення
+    file_url = bot.get_file_url(file_path)
+
+    # Завантаження файлу зображення
+    async with aiohttp.ClientSession() as session:
+        async with session.get(file_url) as response:
+            if response.status == 200:
+                image_data = await response.read()
+
+                with open("image.jpg", 'wb') as new_file:
+                    new_file.write(image_data)
+
+                # Список мемчиків зберігається в текстовому файлі
+                with open('meme.txt', 'r', encoding='UTF-8') as f:
+                    meme = f.read().split('\n')
+
+                image = Image.open("image.jpg")
+
+                font = ImageFont.truetype("arial", 45)
+                drawer = ImageDraw.Draw(image)
+
+                width, height = image.size
+                drawer.text((150, height // 1.2), random.choice(meme), font=font, fill='white')
+
+                image.save('new_img.jpg')
+
+                # Відправлення нового зображення
+                with open('new_img.jpg', 'rb') as new_image:
+                    await message.reply_photo(new_image)
+
+
+@dp.message_handler(content_types=types.ContentTypes.TEXT)
+async def handle_text(message: types.Message):
+    await message.reply('Та фото мені кидай)')
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(dp.start_polling())
+    finally:
+        loop.close()
